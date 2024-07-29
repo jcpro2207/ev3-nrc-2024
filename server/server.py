30ac30ca41d344cdc431c75507cc4b5e33073a3a
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2024-07-29 21:35:14 +0800

    add AI, change camera to server webcam - AI code modified from somewhere online

--- server/server.py
+++ server/server.py
@@ -3,11 +3,33 @@ import mimetypes
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
 
-from flask import Flask, redirect, render_template, url_for
+from numpy.typing import NDArray
+from flask import Flask, redirect, render_template, url_for, Response
+from flask_socketio import SocketIO
+import cv2
+import base64
+
+from concurrent.futures import ThreadPoolExecutor
+
+PORT = 8000
+SOCKETIO_EVENT_NAME = "data-url"
 
 flask_app = Flask(__name__, static_folder="static", template_folder="templates")
+socketio_app = SocketIO(flask_app)
 
-# region routes
+current_data_url = ""
+
+# region ROUTES
+# redirects
+@flask_app.route('/interface/')
+def r_i():
+    return redirect(url_for("interface"))
+
+@flask_app.route('/camera/')
+def r_c():
+    return redirect(url_for("camera"))
+
+
 @flask_app.route("/")
 def index():
     return render_template("index.html")
@@ -19,12 +41,33 @@ def interface():
 @flask_app.route("/camera")
 def camera():
     return render_template("camera.html")
-
-# redirects
-@flask_app.route('/interface/')
-def r_i():
-    return redirect(url_for("interface"))
-@flask_app.route('/camera/')
-def r_c():
-    return redirect(url_for("camera"))
 # endregion
+
+# region AI
+executor = ThreadPoolExecutor(max_workers=1)
+
+def ndarray_to_dataurl(ndarray: NDArray):
+    return "data:image/jpeg;base64," + base64.b64encode(ndarray.tobytes()).decode()
+
+def AI_loop():
+    from AI import run_AI_and_get_frame
+
+    global current_data_url
+
+    while True:
+        frame = run_AI_and_get_frame()
+        (retval, jpg_image) = cv2.imencode(".jpg", frame)
+
+        # skip this frame if image encoding was unsuccessful
+        if retval is False:
+            continue
+
+        current_data_url = ndarray_to_dataurl(jpg_image)
+
+        socketio_app.emit(SOCKETIO_EVENT_NAME, current_data_url)
+
+executor.submit(AI_loop)
+# endregion
+
+if __name__ == "__main__":
+    socketio_app.run(app=flask_app, port=PORT, use_reloader=False, log_output=True)
--- server/static/js/interface.js
+++ server/static/js/interface.js
@@ -1,82 +1,13 @@
 /* eslint-env browser */
-/* global Daily */
+/* global io */
 /* eslint-disable no-console */
 
-import updateStatus from "./updateStatus.js";
+const SOCKETIO_EVENT_NAME = "data-url";
 
-const btnJoin = document.getElementById("btn-join");
-const videoDisplay = document.getElementById("video-display");
+const socket = io();
 
-let connected = false;
-updateStatus("not connected", 0, "not connected", 0);
+const imgEl = document.getElementById("img-el");
 
-const DAILY_ROOM_URL = "https://purplish.daily.co/purplish";
-
-const call = Daily.createCallObject({
-	url: DAILY_ROOM_URL,
-	videoSource: false,
-	audioSource: false,
-});
-
-call.on("loading", () => {
-	console.log("event `loading`");
-
-	updateStatus("connecting...", 1, null, null);
-});
-
-// "[`joined-meeting` event]: The participants list will only ever include the local participant. This is because the server sends participant information in batches of 50 after this initial joined event occurs [...] To get detailed participant information, listen for the `participant-joined` event, sent in conjunction with the batched updates." - daily docs
-call.on("joined-meeting", () => {
-	console.log("event `joined-meeting`");
-	// console.log("local participant object:", ev.participants.local);
-
-	connected = true;
-	updateStatus("connected", 2, "waiting for video...", 1);
-
-	btnJoin.innerText = "Disconnect";
-	btnJoin.disabled = false;
-});
-
-call.on("left-meeting", () => {
-	console.log("event `left-meeting`");
-	updateStatus("disconnected", 0, "disconnected", 0);
-
-	btnJoin.disabled = false;
-	btnJoin.innerText = "Reconnect";
-	connected = false;
-});
-
-call.on("participant-joined", () => {
-	console.log("event `participant-joined`");
-});
-
-call.on("track-started", async (ev) => {
-	console.log("event `track-started`:", ev);
-
-	const { track, participant } = ev;
-	if (track.kind !== "video") return;
-
-	console.log("the participant joined at", participant.joined_at.toLocaleTimeString());
-
-	videoDisplay.srcObject = new MediaStream([track]);
-	updateStatus(null, null, "streaming", 2);
-});
-
-call.on("track-stopped", (ev) => {
-	console.log("event `track-stopped`:", ev);
-
-	updateStatus(null, null, "not streaming", 0);
-});
-
-call.on("error", console.log);
-
-btnJoin.addEventListener("click", () => {
-	console.log("btnJoin clicked");
-
-	if (connected === false) {
-		btnJoin.disabled = true;
-		call.join();
-	} else {
-		btnJoin.disabled = true;
-		call.leave();
-	}
+socket.on(SOCKETIO_EVENT_NAME, (data) => {
+	imgEl.src = data;
 });
